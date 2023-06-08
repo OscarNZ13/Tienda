f84addb9f1a3afc4c05509b400617a9b2cc2de48
OscarNZ13 <133930637+OscarNZ13@users.noreply.github.com>
2023-06-07 20:54:53 -0600

    4to commit
    Semana #4

---
+++ src/main/java/com/Tienda/Tienda/IndexController.java
@@ -0,0 +1,13 @@
+package com.Tienda.Tienda;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//Le dijo a SB que esto es un controller
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+        //Este mae va a ir a retornar lo que hay dentro de templates
+    }
+}
